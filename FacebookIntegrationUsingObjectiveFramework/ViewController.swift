1f9715cb700cf537b0f9c33e2c4c9dd121d63a6b
Mehul Shah <mehul_shah_19@live.com>
2016-09-02 09:04:34 +0530

    Initial Commit with Facebook functionality Added Framework also in the repository

--- FacebookIntegrationUsingObjectiveFramework/ViewController.swift
+++ FacebookIntegrationUsingObjectiveFramework/ViewController.swift
@@ -1,25 +1,122 @@
 //
 //  ViewController.swift
-//  FacebookIntegrationUsingObjectiveFramework
+//  FacebookLoginVeoSoftware
 //
-//  Created by Mehul Shah on 01/09/16.
+//  Created by Mehul Shah on 31/08/16.
 //  Copyright © 2016 Mehul Shah. All rights reserved.
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+     struct FacebookUserFields{
+         static let publicProfile = "public_profile"
+         static let email = "email"
+         static let userFriends = "user_friends"
+         static let firstName = "first_name"
+         static let lastName = "last_name"
+         static let pictureUrl = "picture.type(large)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if(FBSDKAccessToken.currentAccessToken() == nil || FBSDKAccessToken.currentAccessToken() == ""){
+            print("Already login")
+        }
+        let fbsdkLoginButton = FBSDKLoginButton.init()
+        fbsdkLoginButton.readPermissions = [FacebookUserFields.publicProfile,FacebookUserFields.email,FacebookUserFields.userFriends]
+        fbsdkLoginButton.center = view.center
+        fbsdkLoginButton.delegate = self
+        view.addSubview(fbsdkLoginButton)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!){
+  
+        
+        if(error != nil){
+            //TODO: To show user that because of some reason it's being failed
+            return
+        }else{
+            getFacebookUserValues(result)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!){
+        print("Logout\(loginButton)")
+        
+    }
+    
+    func getFacebookUserValues(result : FBSDKLoginManagerLoginResult!){
+        if(result == nil){
+            return
+        }
+       //TODO: save UserID, TokenString ExpirationDate
+        let userID =  result.token.userID
+        let tokenString = result.token.tokenString
+        let expirationDate = result.token.expirationDate
+        
+        
+        var grantedFieldStringSet : Set<String> = Set<String>()
+        //Get Profile and email data
+        if(result.grantedPermissions.contains(FacebookUserFields.email)){
+            grantedFieldStringSet.insert(FacebookUserFields.email)
+        }
+        if(result.grantedPermissions.contains(FacebookUserFields.publicProfile)){
+            grantedFieldStringSet.insert(FacebookUserFields.firstName)
+            grantedFieldStringSet.insert(FacebookUserFields.lastName)
+            grantedFieldStringSet.insert(FacebookUserFields.pictureUrl)
+        }
+        
+//        if(grantedFieldStringSet.isEmpty){
+//            return
+//        }
+//        
+//        var grantedFieldString: String = ""
+//        
+//        for singleGrantedFieldString in grantedFieldStringSet{
+//            grantedFieldString += singleGrantedFieldString + ", "
+//        }
+//        grantedFieldString.removeAtIndex(grantedFieldString.endIndex.predecessor())
+//        grantedFieldString.removeAtIndex(grantedFieldString.endIndex.predecessor())
+//        
+        
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields": "first_name, last_name, picture.type(large), email"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            if(error != nil){
+                return
+            }
+            
+            result.removeObject("first_name");
+            
+            let strFirstName: String = (result.objectForKey(FacebookUserFields.firstName) as? String)!
+            let strLastName: String = (result.objectForKey(FacebookUserFields.lastName) as? String)!
+            let email : String = (result.objectForKey(FacebookUserFields.email) as? String)!
+            let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            print("First Name \(strFirstName)  Last Name \(strLastName) Picture Url \(strPictureURL)")
+        }
+        
 
-
+        if(result.grantedPermissions.contains(FacebookUserFields.email)){
+            
+        }
+        
+    }
+    
+    
+    func facebookLogout(){
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+     }
+    
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FacebookIntegrationUsingObjectiveFramework' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire', '~> 3.4'
+  
+  target 'FacebookIntegrationUsingObjectiveFrameworkTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FacebookIntegrationUsingObjectiveFrameworkUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
